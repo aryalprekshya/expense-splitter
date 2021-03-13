6b09c03a8ddf1b400ceddd9ade23dfc47d176356
aryalprekshya <aryal.prekshyaa@gmail.com>
2021-03-12 20:56:47 -0500

    added add-expense form

--- src/views/AddExpense.jsx
+++ src/views/AddExpense.jsx
@@ -1,5 +1,37 @@
 import React from "react";
+import Header from "../components/component/Header";
 
 export default function AddExpense() {
-  return <div>This is Add expense Page</div>;
+  const handleAddExpense = () => {
+    console.log("Add expense button is clicked");
+  };
+
+  return (
+    <>
+      <div className="container">
+        <div>
+          <form onClick={handleAddExpense}>
+            <div>
+              <label>Description</label>
+              <input type="text" />
+            </div>
+            <div>
+              <label>Amount</label>
+              <input type="text" />
+            </div>
+            <div>
+              <label>Paid By</label>
+              <input type="text" />
+            </div>
+            <div>
+              <label>Paid Date</label>
+              <input type="date" />
+            </div>
+
+            <button>Add Expense</button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
