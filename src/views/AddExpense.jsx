23368e70a29be6beb4525a7baac0d3b883c1c133
aryalprekshya <aryal.prekshyaa@gmail.com>
2021-03-17 20:38:19 -0400

    added bootstrap and reactstrap, added error handling for add-expense page

--- src/views/AddExpense.jsx
+++ src/views/AddExpense.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from "react";
 import ExpenseContext from "../components/context/ExpenseContext";
 import { fs } from "../components/firebase/Firebase";
+import { FormGroup, FormText } from "reactstrap";
 
 export default function AddExpense() {
   const [expense, expenseDispatch] = useContext(ExpenseContext);
@@ -8,6 +9,66 @@ export default function AddExpense() {
   const [amount, setAmount] = useState("");
   const [paidBy, setPaidBy] = useState("");
   const [paidDate, setPaidDate] = useState(new Date());
+
+  //For error handling
+  const [error, setError] = useState({
+    descriptionError: "",
+    amountError: "",
+    paidByError: "",
+    paidDate: "",
+  });
+
+  const handleCheckErrors = (e) => {
+    e.preventDefault();
+    let tempError = {};
+    let hasError = false;
+
+    if (!description) {
+      tempError = {
+        ...tempError,
+        descriptionError: "* Must enter description",
+      };
+      hasError = true;
+    } else {
+      tempError = { ...tempError, descriptionError: "" };
+    }
+
+    if (!amount) {
+      tempError = {
+        ...tempError,
+        amountError: "* Must enter paid amount",
+      };
+      hasError = true;
+    } else {
+      tempError = { ...tempError, amountError: "" };
+    }
+
+    if (!paidBy) {
+      tempError = {
+        ...tempError,
+        paidByError: "* Must enter person's name who paid the amount",
+      };
+      hasError = true;
+    } else {
+      tempError = { ...tempError, paidByError: "" };
+    }
+
+    if (!paidDate) {
+      tempError = {
+        ...tempError,
+        paidDateError: "* Must enter amount paid date",
+      };
+      hasError = true;
+    } else {
+      tempError = { ...tempError, paidDateError: "" };
+    }
+
+    if (hasError) {
+      setError({ ...error, ...tempError });
+    } else {
+      handleAddExpense();
+    }
+  };
 
   const handleAddExpense = (e) => {
     e.preventDefault();
@@ -41,44 +102,53 @@ export default function AddExpense() {
       <div className="container">
         <div>
           <form>
-            <div>
+            <FormGroup>
               <label>Description</label>
               <input
                 type="text"
                 onChange={(e) => {
                   setDescription(e.target.value);
+                  setError({ ...error, descriptionError: "" });
                 }}
               />
-            </div>
-            <div>
+              <FormText>{error.descriptionError}</FormText>
+            </FormGroup>
+            <FormGroup>
               <label>Amount</label>
               <input
-                type="text"
+                type="number"
+                min={1}
                 onChange={(e) => {
                   setAmount(e.target.value);
+                  setError({ ...error, amountError: "" });
                 }}
               />
-            </div>
-            <div>
+              <FormText>{error.amountError}</FormText>
+            </FormGroup>
+            <FormGroup>
               <label>Paid By</label>
               <input
                 type="text"
                 onChange={(e) => {
                   setPaidBy(e.target.value);
+                  setError({ ...error, paidByError: "" });
                 }}
               />
-            </div>
-            <div>
+              <FormText>{error.paidByError}</FormText>
+            </FormGroup>
+            <FormGroup>
               <label>Paid Date</label>
               <input
                 type="date"
                 onChange={(e) => {
                   setPaidDate(e.target.value);
+                  setError({ ...error, paidDateError: "" });
                 }}
               />
-            </div>
+              <FormGroup>{error.paidDateError}</FormGroup>
+            </FormGroup>
 
-            <button onClick={handleAddExpense}>Add Expense</button>
+            <button onClick={handleCheckErrors}>Add Expense</button>
           </form>
         </div>
       </div>
